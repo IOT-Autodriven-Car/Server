788ed2816aca296185a89542104cf14fac75e1ad
vinhnado <vinh.nd0231@gmail.com>
2024-10-19 09:57:10 +0700

    Setup docker

--- backend/lane_detection/consumer.py
+++ backend/lane_detection/consumer.py
@@ -2,6 +2,8 @@ import numpy as np
 import cv2
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
+import os
+from datetime import datetime
 
 
 class VideoStreamConsumer(AsyncWebsocketConsumer):
@@ -15,12 +17,12 @@ class VideoStreamConsumer(AsyncWebsocketConsumer):
         if bytes_data:
             np_arr = np.frombuffer(bytes_data, np.uint8)
             frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
-
             if frame is not None:
                 print("Frame received successfully")
                 result = self.process_frame(frame)
 
                 if result:
+                    print(f"Sending result: {result}")
                     await self.send(text_data=json.dumps({'result': result}))
                 else:
                     await self.send(text_data=json.dumps({'result': None}))
@@ -28,4 +30,18 @@ class VideoStreamConsumer(AsyncWebsocketConsumer):
                 print("Failed to decode frame")
 
     def process_frame(self, frame):
-        return "Processed frame"
+        return "Hello from server"
+
+    def save_frame_to_image(self, frame):
+        timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
+        filename = f"frame_{timestamp}.jpg"
+
+        save_dir = r"/home/vinhnado/Dev/IOT/Server/backend/lane_detection/img"
+        os.makedirs(save_dir, exist_ok=True)
+
+        save_path = os.path.join(save_dir, filename)
+
+        cv2.imwrite(save_path, frame)
+
+        print(f"Frame saved to {save_path}")
+        return save_path
